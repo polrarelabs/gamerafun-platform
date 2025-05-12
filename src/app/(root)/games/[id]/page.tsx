1638186218d30b3983b58ed7bb0fbaeaff263663
LeeSandykv <chinhnguyen170922@gmail.com>
2025-05-12 18:01:54 +0700

    Push game detail

--- src/app/(root)/games/[id]/page.tsx
+++ src/app/(root)/games/[id]/page.tsx
@@ -112,6 +112,7 @@ const LayoutGameDetail = () => {
       <Button
         sx={{
           backgroundColor: "white",
+          color: "black",
           "&:hover": {
             backgroundColor: "gray",
           },
